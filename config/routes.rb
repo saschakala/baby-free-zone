408a58a0e7792bda8bdb7dfb46b617f4c7d7657d
Sascha Kala <mxsaschakala@gmail.com>
2020-11-06 11:54:05 -0700

    added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: "birth_controls#index"
+  root to: "birth_controls#index"
 end
+ 
