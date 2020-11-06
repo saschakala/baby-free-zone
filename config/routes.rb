6bfa91b4115e883c15cb950c2e8e24cf50b76436
Sascha Kala <mxsaschakala@gmail.com>
2020-11-06 13:08:06 -0700

    whitelisted reg controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :birth_controls
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "birth_controls#index"
