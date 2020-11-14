fb7fa73297badb6f0744be6357d7d2c962ce0343
Sascha Kala <mxsaschakala@gmail.com>
2020-11-14 16:07:25 -0500

    add provider and uid to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_200032) do
+ActiveRecord::Schema.define(version: 2020_11_14_210618) do
 
   create_table "birth_controls", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_11_06_200032) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
