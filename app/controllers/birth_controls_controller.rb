784c6fff909ac3ae8cb9082813bced6a16b4c8f4
Sascha Kala <mxsaschakala@gmail.com>
2020-11-15 20:00:41 -0500

    set birth control variables in controller

--- app/controllers/birth_controls_controller.rb
+++ app/controllers/birth_controls_controller.rb
@@ -1,5 +1,23 @@
 class BirthControlsController < ApplicationController
+    before_action :set_birth_control
 
     def index
+        @birthcontrols = BirthControl.all
     end
+
+    def show
+    end
+
+
+    private
+
+    def set_birth_control
+        @bc = BirthControl.find_by_id(params[:id])
+    end
+
+    # def bc_params
+    #     params.require(:birth_control).permit(:name, :method, :active_ingredient, :dosage, :hormone_type, :generic_name)
+    # end
+
+
 end
