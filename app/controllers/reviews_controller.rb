9b0a53d4a6c4f885ef3dbf313255655c3f06120c
Sascha Kala <mxsaschakala@gmail.com>
2020-11-15 23:13:53 -0500

    add RESTful route methods to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,21 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.bc(params[:birth_control_id])
     end
-
     
     def new
     end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
 end
