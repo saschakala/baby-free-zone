1481271a86b5c67919bce784d75145be13f4dd8d
Sascha Kala <mxsaschakala@gmail.com>
2020-11-15 19:47:19 -0500

    added nested resources route for reviews:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :side_effects
-  resources :reviews
+  # resources :side_effects
+
+  resources :birth_controls do
+    resources :reviews
+  end
+
+
+  # resources :reviews
   resources :birth_controls
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
