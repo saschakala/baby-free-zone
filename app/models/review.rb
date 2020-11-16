67b0e3fc5b0021e4855668676f483fa827e794c7
Sascha Kala <mxsaschakala@gmail.com>
2020-11-16 13:45:36 -0500

    added severity to review index show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
   has_many :side_effects, through: :review_side_effects
 
   validates :rating, :inclusion => 1..10
+
+  accepts_nested_attributes_for :review_side_effects
   
   scope :bc, -> (id) { where(birth_control_id: id) }
 
--- app/models/side_effect.rb
+++ app/models/side_effect.rb
@@ -2,4 +2,6 @@ class SideEffect < ApplicationRecord
   validates :name, presence: true
   has_many :review_side_effects
   has_many :reviews, through: :review_side_effects
+
+  accepts_nested_attributes_for :review_side_effects
 end
