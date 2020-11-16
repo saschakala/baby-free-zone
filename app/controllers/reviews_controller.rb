bc459a56d77ff7e9217e20f31709e7eab41dbd81
Sascha Kala <mxsaschakala@gmail.com>
2020-11-15 22:55:39 -0500

    added scope method for reviews by BC type

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
     def index
+        @reviews = Review.bc(params[:birth_control_id])
     end
-    
+
     
     def new
     end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,8 @@ class Review < ApplicationRecord
   belongs_to :birth_control
   has_many :review_side_effects
   has_many :side_effects, through: :review_side_effects
+
   validates :rating, :inclusion => 1..10
+  
+  scope :bc, -> (id) { where(birth_control_id: id) }
 end
