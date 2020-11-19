d15ac3cc356d4229e1e8d35b2dc483bd254b9d03
Sascha Kala <mxsaschakala@gmail.com>
2020-11-19 15:56:40 -0500

    add to reviews controller new and create actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,23 @@
 class ReviewsController < ApplicationController
+    before_action :set_birth_control, only: [:index, :new]
+
     def index
         @reviews = Review.bc(params[:birth_control_id])
-        @bc = BirthControl.find_by_id(params[:birth_control_id])
     end
     
     def new
+        @review = Review.new
+        # 2.times { @review.side_effects.build}
     end
 
     def create
+        @review = Review.new(review_params)
+        if @review.save
+            redirect_to birth_control_review_path(@bc, @review)
+        else
+            render :new
+        end
+
     end
 
     def edit
@@ -23,6 +33,10 @@ class ReviewsController < ApplicationController
 
     def review_params
         params.require(:review).permit(:title, :rating, :description)
+    end
+
+    def set_birth_control
+        @bc = BirthControl.find_by_id(params[:birth_control_id])
     end
 
 
